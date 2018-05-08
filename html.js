ad1b9786d5bd0b57f7f84f2e251b35039b80b593
Philipp Hancke <fippo@andyet.net>
2018-05-08 13:58:46 +0200

    use jsdom instead of broken regexp

--- html.js
+++ html.js
@@ -2,18 +2,25 @@
  * extract script content from a series of html files, run a
  * jscodeshift codemod on them and overwrite the original file.
  */
+const { JSDOM } = require('jsdom');
 const fs = require('fs');
 const {execFileSync} = require('child_process');
-const match = /(<script?>)([\s\S]*?)(<\/script>)/gi;
 
-const codemod = process.argv[2]; // 'wpt-async-test'; // wpt-pass-test';
+const codemod = process.argv[2];
 const filenames = process.argv.slice(3);
 filenames.forEach((filename) => {
     const originalContent = fs.readFileSync(filename, 'utf-8');
-    const script = match.exec(originalContent);
-    if (!script) return;
+    const dom = new JSDOM(originalContent);
+    const document = dom.window.document;
+    const scriptTags = document.querySelectorAll('script');
+    const lastTag = scriptTags[scriptTags.length - 1];
+    const script = lastTag.innerHTML;
+    if (!script) {
+        console.log('NO SCRIPT FOUND', filename);
+        return;
+    }
     const scriptFilename = filename + '.codemod.js';
-    const scriptFile = fs.writeFileSync(scriptFilename, script[2]);
+    const scriptFile = fs.writeFileSync(scriptFilename, script);
     // exec jscodeshift
     const output = execFileSync('./node_modules/.bin/jscodeshift', ['-t', codemod, scriptFilename]);
     console.log(filename, output.toString()); // output jscodeshift output.
@@ -21,7 +28,7 @@ filenames.forEach((filename) => {
     const newScript = fs.readFileSync(scriptFilename, 'utf-8').toString();
 
     // const modifiedContent = originalContent.replace(script[0], script[1] + newScript + script[script.length - 1]);
-    const modifiedContent = originalContent.split(script[0]).join(script[1] + newScript + script[script.length - 1]);
+    const modifiedContent = originalContent.split(script).join(newScript);
     fs.writeFileSync(filename, modifiedContent);
     fs.unlinkSync(scriptFilename);
 });
